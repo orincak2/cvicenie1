8aaaa671c43e5e8d6783539fc4cfc56759bea817
Radovan Orinčák <radovan.orincak@gmail.com>
2020-09-29 00:35:04 +0200

    opravil som chybu v slove "kamen"

--- hanoi.c
+++ hanoi.c
@@ -3,7 +3,7 @@
 void hanoi(int n, int a, int b)
 {
   if (n == 1) 
-    printf("posun kmen 1 z %d do %d\n", a, b);
+    printf("posun kamen 1 z %d do %d\n", a, b);
   else
   {
     hanoi(n - 1, a, 6 - a - b);
